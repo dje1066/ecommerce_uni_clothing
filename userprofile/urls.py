6ed5e6f552d3d9dbfe7abd4a23e3def2b8400273
dje1066 <113864086+dje1066@users.noreply.github.com>
2023-11-03 19:34:16 -0400

    added log in function/page, access through url

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('myaccount/', views.myaccount, name='myaccount'),
 
