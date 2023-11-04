9a6207e0ce982447a4bd3cebd30bb742c90903c2
dje1066 <113864086+dje1066@users.noreply.github.com>
2023-11-04 16:55:58 -0400

    added temporary images to product frontpage + product view

--- storeproject/urls.py
+++ storeproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from store.views import frontpage, about
@@ -28,4 +30,4 @@ urlpatterns = [
     path('', include('store.urls')),
     path('', include('product.urls')),
     path('', frontpage, name='frontpage'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
